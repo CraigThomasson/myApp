f6d388b8dbfb8c2dd77fe750ff512b1facdfce21
craig thomasson <c.a.thomasson@hotmail.com>
2024-03-18 13:45:31 +0000

    set up basic set up for andriod

---
+++ android/app/src/main/java/com/craig/test_app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.craig.test_app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appId: 'com.craig.test_app',
+  appName: 'test-app',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
