d181f003a8ce19fc496e3400c233a035b2a7eedd
craig thomasson <c.a.thomasson@hotmail.com>
2024-05-05 00:16:28 +0100

    add script to generate dice roll and click

--- src/components/containers/DisplayDiceResult.tsx
+++ src/components/containers/DisplayDiceResult.tsx
@@ -1,14 +1,20 @@
+import React, { useState } from 'react';
 import './ExploreContainer.css';
+import BaseButton from '../../buttons/baseButton';
+import rollDice from '../../scripts/rollDice';
 
 interface ContainerProps {
     name: string;
 }
 
-const number = Math.floor(Math.random() * 6) + 1;
 const DisplayDiceResult: React.FC<ContainerProps> = ({ name }) => {
+    const [number, setNumber] = useState(0);
     return (
         <div className="container">
             <strong>{name}</strong>
+            <div>
+                <BaseButton onClick={() => setNumber(rollDice(8,8))} text='Roll Dice' />
+            </div>
             <p>{number}</p>
          
         </div>
